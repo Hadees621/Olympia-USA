5b0a307f5ca3334375235d2b473af1cbeee8299a
hadees621 <hadees621@gmail.com>
2024-03-11 18:55:54 +0500

    routing fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen min-w-md flex-col items-center justify-between">
-      <div className="h-96 bg-red-200 w-full">
-        OLYMPIA PUBLISHERS
-        OLYMPIA PUBLISHERS
-      </div>
-      <div className="h-96 bg-red-200 w-full">
-        OLYMPIA PUBLISHERS
-        OLYMPIA PUBLISHERS
-      </div>
-      <div className="h-96 bg-red-200 w-full">
-        OLYMPIA PUBLISHERS
-        OLYMPIA PUBLISHERS
-      </div>
-      <div className="h-96 bg-red-500 w-full">
-        OLYMPIA PUBLISHERS
-        OLYMPIA PUBLISHERS
-      </div>
-      <div className="h-96 bg-red-500 w-full">
-        OLYMPIA PUBLISHERS
-        OLYMPIA PUBLISHERS
-      </div>
-
-    </main>
-  );
+    <div>
+      <h1>HOME</h1>
+    </div>
+  )
 }
