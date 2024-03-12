6416431dd4e482ef9db08428b5c6489ae225b304
hadees621 <hadees621@gmail.com>
2024-03-12 10:47:05 +0500

    files

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,10 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Head>
-        <link rel="icon" href="/public/assests/Icons/Olympialogomark.png" />
-      </Head> */}
-      <body className={inter.className}>{children}</body>
+      <body className="h-screen">
+        {children}
+      </body>
     </html>
   );
 }
