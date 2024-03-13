e6de974ab4bbfbe4b6d07a29f436ac4b7921db13
hadees621 <hadees621@gmail.com>
2024-03-13 13:42:42 +0500

    hover effect

--- src/components/home/BookComponent.js
+++ src/components/home/BookComponent.js
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 const BookComponent = ({ src, title, author }) => {
     return (
-        <div className='p-10 flex flex-col border border-black flex-grow'>
+        <div className='p-10 flex flex-col border border-black flex-grow hover:bg-[#EFF4F7]'>
             <div lassName=' border'>
-                <Image src={src} width={150} height={400} />
+                <Image src={src} width={150} height={400} className='hover:shadow'/>
             </div>
             <div className='pt-14 font-droid-sans space-y-2 text-[12px]'>
                 <p className='font-semibold'>{title}</p>
