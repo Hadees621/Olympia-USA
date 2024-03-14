ea1c1e18894cfdeecddf2c512f5c1a82a9219497
hadees <hadees621@gmail.com>
2024-03-14 16:09:43 +0500

    chore: categories

---
+++ src/components/home/Categories.js
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="">
+      <p className="text-[26px] font-semibold font-droid-sans text-start ">
+        Categories
+      </p>
+      <div className="flex gap-20 py-20">
+        <div className="border bg-blue-800 h-[340px] flex flex-col justify-between">
+          <div className=" text-black font-open-sans">
+            <h2 className="text-xl font-semibold text-gray-200 p-6">Mystery</h2>
+          </div>
+          <Image
+            src="/assests/Home/cat1.png"
+            width={300}
+            height={500}
+            alt="BumblebeeBanner"
+          />
+        </div>
+        <div className="border bg-gray-200 h-[340px] flex flex-col justify-between">
+          <div className=" text-black font-open-sans">
+            <h2 className="text-xl font-semibold text-blue-800 p-6">Mystery</h2>
+          </div>
+          <Image
+            src="/assests/Home/cat2.png"
+            width={300}
+            height={500}
+            alt="BumblebeeBanner"
+          />
+        </div>
+        <div className="border bg-gray-200 h-[340px] flex flex-col justify-between">
+          <div className=" text-black font-open-sans">
+            <h2 className="text-xl font-semibold text-blue-800 p-6">Mystery</h2>
+          </div>
+          <Image
+            src="/assests/Home/cat3.png"
+            width={300}
+            height={500}
+            alt="BumblebeeBanner"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
