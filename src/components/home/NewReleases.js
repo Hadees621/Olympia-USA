5d1e8bb278629d487d5e9f7d3b97c466a5229b0f
hadees <hadees621@gmail.com>
2024-03-15 15:57:40 +0500

    fix: reps

--- src/components/home/NewReleases.js
+++ src/components/home/NewReleases.js
@@ -7,14 +7,19 @@ const NewReleases = () => {
   return (
     <div className="lg:max-w-[800px] xl:max-w-[1300px]">
       <div className="flex justify-center lg:justify-between items-center py-10 px-5 lg:px-0 lg:bg-white">
-        <p className="text-[26px] font-semibold font-droid-sans">
-          New Releases
-        </p>
+        <div>
+          <p className="text-[26px] font-semibold font-droid-sans hidden lg:block">
+            New Releases
+          </p>
+          <p className="text-[26px] font-semibold font-open-sans lg:hidden">
+            NEW RELEASES
+          </p>
+        </div>
         <div className="hidden lg:block">
           <Button title="EXPLORE MORE" width="w-[150px]" color="transparent" />
         </div>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-0 lg:flex items-center justify-center lg:border lg:border-black">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-5 lg:gap-0 lg:flex items-center justify-center lg:border lg:border-black mx-5 lg:mx-0">
         {Books.map((book, index) => (
           <div className="">
             <BookComponent
@@ -25,6 +30,13 @@ const NewReleases = () => {
             />
           </div>
         ))}
+      </div>
+      <div className="w-full flex justify-center lg:hidden">
+        <button
+          className={`text-[14px] py-[12px] font-semibold font-open-sans shadow flex items-center justify-center border border-buttonred text-buttonred px-3 w-[170px] my-7 `}
+        >
+          Explore more
+        </button>
       </div>
     </div>
   );
