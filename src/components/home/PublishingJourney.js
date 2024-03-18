0c35082a2651d23a04a57aad149784f11ac9f1d3
hadees <hadees621@gmail.com>
2024-03-18 15:11:15 +0500

    section: sm publishing

--- src/components/home/PublishingJourney.js
+++ src/components/home/PublishingJourney.js
@@ -5,14 +5,14 @@ import Button from "../common/Button";
 const PublishingJourney = () => {
   return (
     <div className="flex flex-col lg:max-w-[800px] 2xl:max-w-[1300px] my-20">
-      <div className="flex">
+      <div className="flex flex-col lg:flex-row">
         <div className="flex-1">
-          <div className="flex flex-col h-full justify-center items-center lg:items-start font-open-sans p-5 md:px-10 2xl:p-20 lg:gap-10">
+          <div className="flex flex-col h-full md:justify-center md:items-center lg:items-start lg:justify-start font-open-sans p-5 md:px-10 lg:px-5 2xl:p-20 lg:gap-10">
             <div className="flex flex-col gap-4 mb-5 lg:mb-0">
-              <h2 className="text-[38px] capitalize lg:text-[28px] font-semibold text-buttonred text-center lg:text-start">
+              <h2 className="text-[38px] capitalize lg:text-[28px] font-semibold text-buttonred md:text-center lg:text-start">
                 Start your publishing journey with us
               </h2>
-              <p className="mt-2 font-droid text-[12px] leading-7 lg:pr-10 text-black text-center lg:text-start">
+              <p className="mt-2 font-droid text-[12px] leading-7 lg:pr-10 text-black md:text-center lg:text-start">
                 Have you written a story and looking to get published? Olympia
                 Publishers are now accepting submissions across various genres.
                 Submit your manuscript for us to review.
@@ -21,6 +21,41 @@ const PublishingJourney = () => {
             <Button title="SUBMIT ONLINE" width="w-[130px]" color="red" />
           </div>
         </div>
+        <div className="flex gap-5 px-5 md:justify-center lg:hidden">
+          <div className="flex flex-col lg:flex-row gap-3">
+            <div className="">
+              <Image
+                src={"/Home/Mobile/Asset 31.png"}
+                width={200}
+                height={400}
+              />
+            </div>
+            <div className=" ">
+              <Image
+                src={"/Home/Mobile/Asset 32.png"}
+                width={200}
+                height={200}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <div className=" h-full">
+              <Image
+                src={"/Home/Mobile/Asset 33.png"}
+                width={200}
+                height={400}
+              />
+            </div>
+            <div className="">
+              <Image
+                src={"/Home/Mobile/Asset 34.png"}
+                width={200}
+                height={900}
+              />
+            </div>
+          </div>
+        </div>
+
         <div className="flex-1 hidden lg:flex">
           <Image
             src="/assests/Banners/CSRBanner.png"
