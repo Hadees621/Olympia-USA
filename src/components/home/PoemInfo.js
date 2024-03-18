dc095ead28915653e1b9fab34b85227a72e7c5a1
hadees <hadees621@gmail.com>
2024-03-18 11:39:48 +0500

    fix: missing section

--- src/components/home/PoemInfo.js
+++ src/components/home/PoemInfo.js
@@ -3,7 +3,7 @@ import Button from "../common/Button";
 
 const PoemInfo = ({ title, author }) => {
   return (
-    <div className="flex-1 flex items-center lg:border border-black">
+    <div className="flex-1 flex items-center lg:border border-black py-4 lg:py-0">
       <div className="flex justify-between items-center lg:items-start p-4 lg:flex-col lg:justify-start font-open-sans lg:px-10 gap-6 w-full">
         <div className="flex flex-col">
           <h2 className="text-xl lg:text-3xl font-bold">{title}</h2>
--- src/components/home/PoemsComponent.js
+++ src/components/home/PoemsComponent.js
@@ -3,12 +3,24 @@ import PoemInfo from "./PoemInfo";
 
 const PoemsComponent = () => {
   return (
-    <div className="flex flex-col lg:max-w-[800px] 2xl:max-w-[1200px] lg:border my-20">
+    <div className="flex flex-col lg:max-w-[800px] 2xl:max-w-[1200px] lg:border my-20 gap-32 lg:gap-0">
       <div className="flex flex-col-reverse lg:flex-row bg-[#F9F9F9]">
         <PoemInfo
           author="Victoria Leigh Bennett"
           title="Poems From the Northeast"
         />
+        <div className="flex-1">
+          <Image
+            src="/assests/Home/poems.PNG"
+            width={700}
+            height={200}
+            alt="About Section Image"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col-reverse lg:flex-row bg-[#F9F9F9] lg:hidden">
+        <PoemInfo author="Kristen Mackenzie" title="To Heal the Hurt" />
         <div className="flex-1">
           <Image
             src="/assests/Home/poems.PNG"
