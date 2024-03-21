361c4a0024e3082bd4d047a57aa74f80e7014bfa
hadees <hadees621@gmail.com>
2024-03-21 17:42:29 +0500

    feature: lg screen resp

--- src/app/search-results/page.js
+++ src/app/search-results/page.js
@@ -5,42 +5,56 @@ import React from "react";
 
 const SearchResults = () => {
   return (
-    <div className="mx-5">
-      <div className="bg-[#FAFAFA] p-7 my-6">
-        <h1 className="font-semibold text-[#505050] text-[28px]">
-          Search results
-        </h1>
-      </div>
-      <div className="flex justify-between font-semibold p-8">
-        <p>All</p>
-        <p>Books</p>
-        <p>Authors</p>
-      </div>
-      <p className="font-bold text-center my-8">(6) Books</p>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-5 lg:gap-0 lg:flex items-center justify-center lg:border lg:border-black mx-5 lg:mx-0">
-        {Books.map((book, index) => (
-          <div className="">
-            <BookComponent
-              key={index}
-              src={book.src}
-              title={book.title}
-              author={book.author}
-            />
+    <div className="w-full flex justify-center">
+      <div className="lg:max-w-[800px] 2xl:max-w-[1200px]">
+        <div className="bg-[#FAFAFA] p-7 my-6">
+          <h1 className="font-semibold text-[#505050] text-[28px]">
+            Search results
+          </h1>
+        </div>
+        <div className="flex flex-col font-semibold py-8">
+          <div className="flex justify-between md:justify-center md:gap-6 xl:gap-14">
+            <p>All</p>
+            <p>Books</p>
+            <p>Authors</p>
           </div>
-        ))}
-      </div>
-      <p className="font-bold text-center my-8">(4) Authors</p>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-5 lg:gap-0 lg:flex items-center justify-center lg:border lg:border-black mx-5 lg:mx-0 my-8">
-        {Books.map((book, index) => (
-          <div className="">
-            <AuthorComponent
-              key={index}
-              src={book.src}
-              title={book.title}
-              author={book.author}
-            />
-          </div>
-        ))}
+          <p className="font-bold text-center lg:text-start my-8 lg:hidden">
+            (6) Books
+          </p>
+          <p className="font-semibold text-center lg:text-start mt-10 hidden lg:block text-xl ">
+            Books(6)
+          </p>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5 lg:gap-0 lg:flex items-center justify-center lg:border-black mx-5 lg:mx-0">
+          {Books.map((book, index) => (
+            <div className="">
+              <BookComponent
+                key={index}
+                src={book.src}
+                title={book.title}
+                author={book.author}
+              />
+            </div>
+          ))}
+        </div>
+        <p className="font-bold text-center lg:text-start my-8 lg:hidden">
+          (6) Books
+        </p>
+        <p className="font-semibold text-center lg:text-start mt-24 hidden lg:block text-xl">
+          Authors(6)
+        </p>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5 lg:gap-0 lg:flex items-center justify-center lg:border-black mx-5 lg:mx-0 my-8">
+          {Books.map((book, index) => (
+            <div className="">
+              <AuthorComponent
+                key={index}
+                src={book.src}
+                title={book.title}
+                author={book.author}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
