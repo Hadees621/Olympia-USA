5859942181681c07278bcfe045ff059a22264d1e
hadees <hadees621@gmail.com>
2024-03-25 13:14:56 +0500

    ALL Done

--- src/app/feature/page.js
+++ src/app/feature/page.js
@@ -2,12 +2,13 @@ import { buttonsData } from "@/utils/feature/utils";
 import NavigationButtons from "@/components/feature/NaviagationButtons";
 import RecentPost from "@/components/feature/RecentPost";
 import Features from "@/components/feature/Features";
+import SmPagination from "@/components/feature/SmPagination";
+import LgPagination from "@/components/feature/LgPagination";
 
 const Page = () => {
   return (
     <div className="w-full lg:flex lg:justify-center">
       <div className="lg:max-w-[850px] xl:max-w-[1000px] 2xl:max-w-[1400px]">
-        {/* Features section */}
         <div className="bg-[#FAFAFA] p-7 my-6 mx-5 lg:mx-0">
           <h1 className="font-semibold text-[#505050] text-[28px]">Features</h1>
         </div>
@@ -17,6 +18,10 @@ const Page = () => {
         <RecentPost />
 
         <Features key={1} />
+
+        <SmPagination />
+
+        <LgPagination />
       </div>
     </div>
   );
---
+++ src/components/feature/LgPagination.js
@@ -0,0 +1,90 @@
+import React from "react";
+import Button from "./Button";
+
+const LgPagination = () => {
+  return (
+    <div className="lg:flex w-full justify-end items-center py-8 hidden">
+      <div aria-label="Page navigation example " className="flex w-1/2">
+        <div>
+          <ul class="flex items-center -space-x-px h-8 text-sm">
+            <li>
+              <a
+                href="#"
+                class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                1
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                2
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                aria-current="page"
+                class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+              >
+                3
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                4
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                aria-current="page"
+                class="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+              >
+                5
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              >
+                6
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="bg-white rounded-full flex items-center justify-center mx-5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="2em"
+            height="2em"
+            viewBox="0 0 26 26"
+          >
+            <g fill="currentColor">
+              <path d="M10.116 7.32a.5.5 0 1 1 .768-.64l5 6a.5.5 0 0 1-.768.64z" />
+              <path d="M10.884 19.32a.5.5 0 0 1-.768-.64l5-6a.5.5 0 1 1 .768.64z" />
+              <path
+                fill-rule="evenodd"
+                d="M13 24.5c6.351 0 11.5-5.149 11.5-11.5S19.351 1.5 13 1.5S1.5 6.649 1.5 13S6.649 24.5 13 24.5m0 1c6.904 0 12.5-5.596 12.5-12.5S19.904.5 13 .5S.5 6.096.5 13S6.096 25.5 13 25.5"
+                clip-rule="evenodd"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+      <button
+        className={`text-[10px] py-2 px-7 font-semibold font-open-sans shadow border border-black flex items-center justify-center w-max my-7`}
+      >
+        LOAD MORE
+      </button>
+    </div>
+  );
+};
+
+export default LgPagination;
