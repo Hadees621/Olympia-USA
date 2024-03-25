45ceea11e60f4a541c72e6f52e2360d2b6b19b7f
hadees <hadees621@gmail.com>
2024-03-25 12:01:03 +0500

    feature: for lg screen

--- src/app/feature/page.js
+++ src/app/feature/page.js
@@ -6,39 +6,30 @@ import Button from "@/components/feature/Button";
 const Page = () => {
   return (
     <div className="w-full lg:flex lg:justify-center">
-      <div className="lg:max-w-[800px] 2xl:max-w-[1200px]">
+      <div className="lg:max-w-[850px] xl:max-w-[1000px] 2xl:max-w-[1400px]">
         {/* Features section */}
         <div className="bg-[#FAFAFA] p-7 my-6 mx-5 lg:mx-0">
           <h1 className="font-semibold text-[#505050] text-[28px]">Features</h1>
         </div>
 
         {/* Navigation buttons */}
-        <div className="overflow-x-auto py-6 ml-5">
-          <div className="flex justify-center gap-6 xl:gap-14 w-max">
+        <div className="overflow-x-auto py-6 xl:py-2 ml-5">
+          <div className="flex justify-between 2xl:justify-center 2xl:gap-14 gap-6 w-full">
             {buttonsData.map((button, index) => (
-              <Button
+              <button
                 key={index}
-                title={button.title}
-                width="w-max"
-                color="transparent"
-              />
+                className={`text-[14px] 2xl:text-[18px] tp-3 xl:p-1 2xl:p-3 font-semibold font-droid-sans hover:shadow flex items-center justify-center hover:border hover:border-red-300 w-max hover:text-[#C9444F] my-7`}
+              >
+                {button.title}
+              </button>
             ))}
           </div>
         </div>
 
         {/* Article section */}
-        <div className="flex flex-col lg:flex-row py-10 mx-3">
+        <div className="grid lg:grid-cols-3 md:justify-center gap-4 lg:gap-0 lg:flex-row py-10 mx-3">
           {articlesData.map((article, index) => (
-            <Article
-              key={index}
-              src={article.src}
-              width={article.width}
-              height={article.height}
-              title={article.title}
-              date={article.date}
-              category={article.category}
-              content={article.content}
-            />
+            <Article key={index} {...article} />
           ))}
         </div>
 
--- src/utils/feature/utils.js
+++ src/utils/feature/utils.js
@@ -38,4 +38,34 @@ export const articlesData = [
     content:
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy...",
   },
+  {
+    src: "/Home/Asset 4.png",
+    width: 450,
+    height: 190,
+    title: "ARTICLE TITLE 1",
+    date: "July 01, 2022",
+    category: "Category",
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy...",
+  },
+  {
+    src: "/Home/Asset 5.png",
+    width: 450,
+    height: 190,
+    title: "ARTICLE TITLE 2",
+    date: "July 01, 2022",
+    category: "Category",
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy...",
+  },
+  {
+    src: "/Home/Asset 6.png",
+    width: 450,
+    height: 190,
+    title: "ARTICLE TITLE 3",
+    date: "July 01, 2022",
+    category: "Category",
+    content:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy...",
+  },
 ];
