4403640b0109563829878609e61c149c29e298a5
hadees <hadees621@gmail.com>
2024-03-26 14:42:17 +0500

    addition: state managment

--- src/stores/States.js
+++ src/stores/States.js
@@ -1,5 +1,6 @@
 import create from "zustand";
 
+// small screen navbar
 export const useSmNavbarStore = create((set) => ({
   menuOpen: false,
   booksMenuOpen: false,
@@ -10,6 +11,7 @@ export const useSmNavbarStore = create((set) => ({
   toggleSearchMenu: () => set((state) => ({ searchOpen: !state.searchOpen })),
 }));
 
+// Normal screen Navbar
 export const useNavbarStore = create((set) => ({
   booksDropdownOpen: false,
   searchDropdownOpen: false,
@@ -17,4 +19,21 @@ export const useNavbarStore = create((set) => ({
     set((state) => ({ booksDropdownOpen: !state.booksDropdownOpen })),
   toggleSearchDropdown: () =>
     set((state) => ({ searchDropdownOpen: !state.searchDropdownOpen })),
+}));
+
+//  sortby dropdown
+
+export const useSortByStore = create((set) => ({
+  showSortOptions: false,
+  showDropdown1: false,
+  showDropdown2: false,
+  showDropdown2: false,
+  setShowSortOptions: () =>
+    set((state) => ({ showSortOptions: !state.showSortOptions })),
+  toggleDropdown1: () =>
+    set((state) => ({ showDropdown1: !state.showDropdown1 })),
+  toggleDropdown2: () =>
+    set((state) => ({ showDropdown2: !state.showDropdown2 })),
+  toggleDropdown3: () =>
+    set((state) => ({ showDropdown3: !state.showDropdown3 })),
 }));
