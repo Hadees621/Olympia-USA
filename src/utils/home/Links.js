ec480b6d4f5c79e49624cca6484205c1eab8ddd3
hadees <hadees621@gmail.com>
2024-04-01 16:41:06 +0500

    chore: navbar links added

--- src/utils/home/Links.js
+++ src/utils/home/Links.js
@@ -3,3 +3,10 @@ export const paragraph =
   "Olympia Publishers USA is a leading independent book publisher. Publishing talented authors from across the world.We publish adult and children’s fiction and non - fiction books in various formats including paperback, hardback, ebook and audiobook.Make your dream a reality, become a published author. Start your publishing journey with us";
 
 export const BumblebeeBanner = "/assests/Banners/BumblebeeBanner.png";
+
+export const navItems = [
+  { label: "AUTHORS", url: "/authors" },
+  { label: "FEATURES", url: "/features" },
+  { label: "ABOUT US", url: "/about" },
+  { label: "PUBLISH WITH US", url: "/publish" },
+];
