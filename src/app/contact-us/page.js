be4f3bc5bff6075dc2f5bf3eea917edf37e3beb2
hadees <hadees621@gmail.com>
2024-04-04 16:26:36 +0500

    feature: contact us - sm

---
+++ src/app/contact-us/page.js
@@ -0,0 +1,14 @@
+import Banner from "@/components/common/Banner";
+
+const page = () => {
+  return (
+    <div className="lg:max-w-[800px] xl:max-w-[1000px] 2xl:max-w-[1350px] mx-auto">
+      <Banner title={"Contact us"} />
+
+      <div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
