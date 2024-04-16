efa817f7402e04cbebd4cf056753d1b10a9b21cc
hadees <hadees621@gmail.com>
2024-04-16 10:21:09 +0500

    fix: hashtag images

--- src/components/home/HashtagSection.js
+++ src/components/home/HashtagSection.js
@@ -6,14 +6,12 @@ const HashtagSection = () => {
     const images = [];
     for (let i = 0; i < count; i++) {
       images.push(
-        <div key={i} className="p-4 border border-black w-max">
-          <Image
-            src={`/Home/Asset 12.png`}
-            width={200}
-            height={200}
-            alt="hashtag"
-          />
-        </div>
+        <img
+          key={i}
+          src={`/Home/Asset 12.png`}
+          alt="hashtag"
+          className="w-[100px] h-[100px] lg:w-[200px] lg:h-[200px] p-2 border border-black"
+        />
       );
     }
     return images;
@@ -21,62 +19,19 @@ const HashtagSection = () => {
 
   return (
     <div className="flex flex-col pt-10 lg:pt-20">
-      <p className="text-[26px] font-semibold font-droid-sans py-5 lg:py-10 lg:pl-80 text-center lg:text-start">
+      <p className="text-[26px] font-semibold font-droid-sans py-5 lg:py-10 lg:pl-60 text-center lg:text-start">
         #Olympiapublishersusa
       </p>
 
-      <div className="grid lg:hidden">
-        <div className="flex">
-          <div className="border-black">
-            <img
-              src={`/Home/Mobile/Asset 38.png`}
-              style={{ height: "250px" }}
-              alt="hashtag"
-            />
-          </div>
-          <div className="border-black">
-            <img
-              src={`/Home/Mobile/Asset 39.png`}
-              style={{ height: "250px" }}
-              alt="hashtag"
-            />
-          </div>
-          <div className="border-black">
-            <img
-              src={`/Home/Mobile/Asset 42.png`}
-              style={{ height: "250px" }}
-              alt="hashtag"
-            />
+      <div className="overflow-hidden relative">
+        <div className="flex justify-center">
+          <div className="flex overflow-x-hidden">
+            <div className="flex flex-col">
+              <div className="w-full flex">{renderImages(8)}</div>
+              <div className="w-full flex">{renderImages(8)}</div>
+            </div>
           </div>
         </div>
-        <div className="flex">
-          <div className="border-black">
-            <img
-              src={`/Home/Mobile/Asset 38.png`}
-              style={{ height: "250px" }}
-              alt="hashtag"
-            />
-          </div>
-          <div className="border-black">
-            <img
-              src={`/Home/Mobile/Asset 39.png`}
-              style={{ height: "250px" }}
-              alt="hashtag"
-            />
-          </div>
-          <div className="border-black">
-            <img
-              src={`/Home/Mobile/Asset 42.png`}
-              style={{ height: "250px" }}
-              alt="hashtag"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="hidden lg:block  overflow-x-scroll">
-        <div className="w-full flex">{renderImages(8)}</div>
-        <div className="w-full flex">{renderImages(8)}</div>
       </div>
     </div>
   );
