c7eed88c27b771ea2f1ee8221db6ae90ba83a4b9
hadees <hadees621@gmail.com>
2024-04-17 17:12:21 +0500

    fix: util converted to json

--- src/components/contact-us/SmScreenSections.js
+++ src/components/contact-us/SmScreenSections.js
@@ -3,16 +3,17 @@ import CommonSection from "./CommonSection";
 import LocationCard from "./LocationCard";
 import Review from "./Review";
 import CountrySelector from "./CountrySelector";
-import { addressInfo } from "@/utils/contact-us/utils";
+import ContactUsData from "@/utils/contact-us/ContactUsData.json";
 
 const SmScreenSections = () => {
+  const { addressInfo, commonSectionData } = ContactUsData;
+
   return (
     <div className="lg:hidden">
       <CountrySelector />
 
       {addressInfo.map((info, index) => (
         <LocationCard
-          i={index}
           key={index}
           countryName={info.country}
           imgSrc={info.imgSrc}
@@ -24,20 +25,16 @@ const SmScreenSections = () => {
 
       <Review />
 
-      <CommonSection
-        title="START YOUR PUBLISHING JOURNEY WITH US"
-        content="Olympia Publishers are now accepting submissions across various genres. If you have written a story, submit your manuscript for us to review."
-        buttonTitle="Read our submissions guidelines"
-        buttonWidth="w-[240px] h-[44px] lg:w-[130px] lg:h-[38px] 2xl:w-[130px] 2xl:h-[45px] text-[13px] border border-white mt-6"
-        bgColor="bg-[#C61E2B]"
-      />
-      <CommonSection
-        title="FAQS"
-        content="For anymore questions or queries, visit our frequently asked questions page. We are ready to answer all of your questions!"
-        buttonTitle="View FAQs"
-        buttonWidth="w-[100px] h-[44px] lg:w-[130px] lg:h-[38px] 2xl:w-[130px] 2xl:h-[45px] text-[13px] border border-white mt-6"
-        bgColor="bg-black"
-      />
+      {commonSectionData.map((section, index) => (
+        <CommonSection
+          key={index}
+          title={section.title}
+          content={section.content}
+          buttonTitle={section.buttonTitle}
+          buttonWidth={section.buttonWidth}
+          bgColor={section.bgColor}
+        />
+      ))}
     </div>
   );
 };
