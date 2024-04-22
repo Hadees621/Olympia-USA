85d5ee9deb043d369d32bd94dad6cd7eee10fffe
hadees <hadees621@gmail.com>
2024-04-22 19:21:42 +0500

    fix

--- src/app/publishing-process/page.js
+++ src/app/publishing-process/page.js
@@ -9,7 +9,7 @@ const { publishing } = SectionsData;
 const Page = () => {
   return (
     <>
-      <div className="max-w-[800px] mx-auto lg:max-w-[1350px]">
+      <div className="lg:max-w-[800px] xl:max-w-[1000px] 2xl:max-w-[1350px] mx-auto">
         <Banner title={"Publishing process"} />
         <div className="text-[13px] text-center xl:text-[13px] 2xl:text-[17px] space-y-4 md:space-y-2 xl:space-y-4 leading-9 md:leading-8 xl:leading-7 2xl:leading-9 md:font-droid-sans mx-7 md:text-[14px] md:max-w-[400px] md:mx-auto lg:text-start lg:max-w-[550px] xl:max-w-[700px] 2xl:max-w-[800px] lg:my-16">
           We know that getting your book published can sometimes seem like a
