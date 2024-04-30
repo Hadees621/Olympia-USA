5a6bac48714fe928b58f683afd5085e724bea904
hadees <hadees621@gmail.com>
2024-04-30 16:33:25 +0500

    book comp

--- src/components/home/BookComponent.js
+++ src/components/home/BookComponent.js
@@ -11,7 +11,7 @@ const BookComponent = ({ src, title, author, bumblebee, bookId }) => {
 
   return (
     <div
-      className={`flex flex-col lg:border lg:border-black flex-grow items-center justify-start h-[350px] lg:h-[250px] xl:h-[330px] 2xl:h-[440px] ${backgroundColor}`}
+      className={`flex flex-col lg:border lg:border-black flex-grow items-center justify-start h-[350px] lg:h-[250px] xl:h-[340px] 2xl:h-[440px] ${backgroundColor} lg:p-2 xl:p-4`}
     >
       <Link href={`/books/${bookId}`}>
         <div
@@ -29,9 +29,9 @@ const BookComponent = ({ src, title, author, bumblebee, bookId }) => {
           </div>
         </div>
       </Link>
-      <div className="p-4 lg:p-3 xl:p-5 2xl:p-7 w-full">
+      <div className="p-4 lg:p-3 2xl:p-7 w-full">
         <p className="font-semibold font-driod-sans lg:text-[10px] xl:text-[12px] 2xl:text-[15px] uppercase">
-          {title + title }
+          {title + title}
         </p>
         <p className="text-[#616161] font-open-sans lg:text-[10px] xl:text-[11px] 2xl:text-[14px]">
           {author + author}
@@ -42,36 +42,3 @@ const BookComponent = ({ src, title, author, bumblebee, bookId }) => {
 };
 
 export default BookComponent;
-
-// import React from "react";
-// import Link from "next/link";
-
-// const BookComponent = ({ src, title, author, bumblebee, bookId }) => {
-//   const backgroundColor = bumblebee
-//     ? "lg:hover:bg-[#FEEDB6]"
-//     : "lg:hover:bg-[#EFF4F7]";
-
-//   return (
-//     <div
-//       className={`flex flex-col lg:border border-black flex-grow p-4 xl:p-[30px] 2xl:p-[59px] h-[350px] lg:h-[250px] xl:h-[330px] 2xl:h-[440px] ${backgroundColor}`}
-//     >
-//       <Link href={`/books/${bookId}`}>
-//         <div className="bg-[#F6F6F6] lg:bg-transparent overflow-x-auto w-full">
-//           <div className="p-6 lg:p-[4px]">
-//             <img
-//               src={src}
-//               alt="book"
-//               className="w-[140px] h-[210px] lg:w-[135px] lg:h-[180px] xl:w-[137px] 2xl:w-[159px] 2xl:h-[245px]"
-//             />
-//           </div>
-//         </div>
-//       </Link>
-//       <div className="py-4 w-full">
-//         <p className="font-semibold font-driod-sans text-[15px]">{title}</p>
-//         <p className="text-[#616161] font-open-sans text-[14px]">{author}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default BookComponent;
--- src/components/home/MostPopular.js
+++ src/components/home/MostPopular.js
@@ -6,27 +6,27 @@ import Image from "next/image";
 
 const MostPopular = () => {
   return (
-    <div className="lg:max-w-[850px] xl:max-w-[1000px] 2xl:max-w-[1400px] py-20">
+    <div className="lg:max-w-[865px] xl:max-w-[1400px] 2xl:max-w-[1400px] py-20">
       <div className="flex justify-between items-center py-10">
         <p className="text-[26px] font-semibold font-droid-sans">
           Most popular
         </p>
-        <Button title="EXPLORE MORE" width="w-[120px]" color="transparent" />
+        <Button title="EXPLORE MORE" width="w-[140px]" color="transparent" />
       </div>
-      <div className="flex items-center justify-center border-t border-black">
-        <div className="flex">
+      <div className="flex items-center justify-center border-t border-black ">
+        <div className="flex ">
           <BookComponent
             src="/assets/Books/Asset3.png"
             title="title"
             author="author"
           />
-          <div className="relative w-[505px] xl:w-[820px] xl:h-[] 2xl:w-[1100px] h-[320px] 2xl:h-[450px]">
+          <div className="relative h-[350px] lg:h-[250px] xl:h-[330px] 2xl:h-[440px] w-[540px] xl:w-[595px] 2xl:w-[820px]">
             <Image
               src="/assets/Banners/AuthorBanner.png"
               alt="About Section Image"
               layout="fill"
             />
-            <div className="absolute top-4 left-4 2xl:top-8 2xl:left-6 p-4 space-y-2 text-[#BFE0D9] text-[13px]">
+            <div className="absolute top-4 xl:top-8 left-4 2xl:top-8 2xl:left-6 p-4 space-y-2 text-[#BFE0D9] text-[13px] xl:text-[16px] 2xl:text-[20px] font-open-sans">
               <p className="">Featured author</p>
               <p className="text-white text-xl 2xl:text-3xl ">
                 Legends of Erin: Dawn of <br /> Boyhood’s Fire
