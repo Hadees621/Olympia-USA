3922221939236fbeba2cff3ed3a2ceb7b2c43f41
hadees <hadees621@gmail.com>
2024-05-02 17:10:12 +0500

    steps updated

--- src/utils/publish-with-us/utils.js
+++ src/utils/publish-with-us/utils.js
@@ -24,3 +24,26 @@ export const tabData = {
     ],
   },
 };
+
+export const steps = [
+  {
+    text: "You have submitted your manuscript to us",
+    imageSrc: `/assets/Submissions process/1.png`,
+    positionClasses: "bottom-8 2xl:bottom-16",
+  },
+  {
+    text: "This will be reviewed by our submissions editor at our London office",
+    imageSrc: `/assets/Submissions process/2.png`,
+    positionClasses: "bottom-3 2xl:bottom-9",
+  },
+  {
+    text: "You will hear back from them with their feedback",
+    imageSrc: `/assets/Submissions process/3.png`,
+    positionClasses: "bottom-8 2xl:bottom-16",
+  },
+  {
+    text: "If successful, you will be offered a contract to publish with us",
+    imageSrc: `/assets/Submissions process/4.png`,
+    positionClasses: "bottom-2 xl:bottom-8 2xl:bottom-16",
+  },
+];
