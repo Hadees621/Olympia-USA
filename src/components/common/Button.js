2220a0077be1caa8405e6a0a92133503228a39bf
hadees <hadees621@gmail.com>
2024-05-03 12:04:10 +0500

    bumblebee fixes

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -46,20 +46,3 @@ export default Button;
 
 // export default Button;
 
-// // const Button = ({ title, width, color, flag }) => {
-// //   const widthClass = width;
-// //   const red = "bg-buttonred text-white";
-// //   const transparent = "border border-black hover:bg-buttonred hover:text-white";
-
-// //   return (
-// //     <button
-// //       className={`text-[12px] py-[12px] font-semibold font-open-sans shadow flex items-center justify-center ${
-// //         color === "red" ? red : transparent
-// //       } ${width ? `${widthClass}` : ""}`}
-// //     >
-// //       {title}
-// //     </button>
-// //   );
-// // };
-
-// // export default Button;
