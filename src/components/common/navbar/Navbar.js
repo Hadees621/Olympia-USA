6ec0699e714a438e7a1651cb13635433c63b4a72
hadees <hadees621@gmail.com>
2024-05-06 17:19:42 +0500

    sticky

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -1,4 +1,5 @@
 "use client";
+
 import Image from "next/image";
 import React from "react";
 import Button from "../Button";
@@ -14,52 +15,122 @@ const Navbar = () => {
   const { booksDropdownOpen, toggleBooksDropdown } = useNavbarStore();
 
   return (
-    <div className="flex justify-center items-center relative z-50">
-      <div className="2xl:h-[80px] bg-white lg:max-w-[800px] xl:max-w-[1200px] 2xl:max-w-[1600px] flex justify-between items-center 2xl:py-[60px] relative z-10">
-        <div>
-          <Link href="/">
-            <Image
-              src="/assets/Icons/logo-usa.png"
-              width={150}
-              height={80}
-              alt="logo"
+    <div className="sticky top-0 z-50 ">
+      <div className="flex justify-center items-center relative">
+        <div className="2xl:h-[80px] bg-white lg:max-w-[800px] xl:max-w-[1200px] 2xl:max-w-[1600px] flex justify-between items-center 2xl:py-[60px] relative z-10">
+          <div>
+            <Link href="/">
+              <Image
+                src="/assets/Icons/logo-usa.png"
+                width={150}
+                height={80}
+                alt="logo"
+              />
+            </Link>
+          </div>
+          <div className="w-[400px] xl:w-[500px] 2xl:w-[850px] mx-[60px] xl:mx-[100px] 2xl:mx-[240px] py-1 2xl:py-20 relative z-10">
+            <SearchBar />
+            <div className="flex font-bold gap-10 items-center justify-between text-[8px] xl:text-[10px] 2xl:text-[12px] relative z-10 py-5">
+              <div
+                className={`relative cursor-pointer`}
+                onClick={toggleBooksDropdown}
+              >
+                <div
+                  className={`flex gap-7 items-center ${
+                    booksDropdownOpen ? "text-red-500" : ""
+                  }`}
+                >
+                  BOOKS{" "}
+                  <div>{booksDropdownOpen ? <Upwards /> : <Downwards />}</div>{" "}
+                </div>
+                {booksDropdownOpen && <BookDropdown />}
+              </div>
+              {navItems.map((item, index) => (
+                <a key={index} href={item.url}>
+                  <p className="cursor-pointer">{item.label}</p>
+                </a>
+              ))}
+            </div>
+          </div>
+          <Link href="/submit-online">
+            <Button
+              title="SUBMIT ONLINE"
+              width="lg:w-[130px] 2xl:w-[140px]"
+              color="red"
             />
           </Link>
         </div>
-        <div className="w-[400px] xl:w-[500px] 2xl:w-[850px] mx-[60px] xl:mx-[100px] 2xl:mx-[240px] py-1 2xl:py-20 relative z-10">
-          <SearchBar />
-          <div className="flex font-bold gap-10 items-center justify-between text-[8px] xl:text-[10px] 2xl:text-[12px] relative z-10 py-5">
-            <div
-              className={`relative cursor-pointer`}
-              onClick={toggleBooksDropdown}
-            >
-              <div
-                className={`flex gap-7 items-center ${
-                  booksDropdownOpen ? "text-red-500" : ""
-                }`}
-              >
-                BOOKS{" "}
-                <div>{booksDropdownOpen ? <Upwards /> : <Downwards />}</div>{" "}
-              </div>
-              {booksDropdownOpen && <BookDropdown />}
-            </div>
-            {navItems.map((item, index) => (
-              <a key={index} href={item.url}>
-                <p className="cursor-pointer">{item.label}</p>
-              </a>
-            ))}
-          </div>
-        </div>
-        <Link href="/submit-online">
-          <Button
-            title="SUBMIT ONLINE"
-            width="lg:w-[130px] 2xl:w-[140px]"
-            color="red"
-          />
-        </Link>
       </div>
     </div>
   );
 };
 
 export default Navbar;
+
+// "use client";
+// import Image from "next/image";
+// import React from "react";
+// import Button from "../Button";
+// import { useNavbarStore } from "@/stores/States";
+// import Downwards from "./navbarComps/Downwards";
+// import Upwards from "./navbarComps/Upwards";
+// import BookDropdown from "./navbarComps/BookDropdown";
+// import SearchBar from "./navbarComps/SearchBar";
+// import { navItems } from "@/utils/home/Links";
+// import Link from "next/link";
+
+// const Navbar = () => {
+//   const { booksDropdownOpen, toggleBooksDropdown } = useNavbarStore();
+
+//   return (
+//     <div className="sticky top-0 z-50">
+//       <div className="flex justify-center items-center relative z-50">
+//         <div className="2xl:h-[80px] bg-white lg:max-w-[800px] xl:max-w-[1200px] 2xl:max-w-[1600px] flex justify-between items-center 2xl:py-[60px] relative z-10">
+//           <div>
+//             <Link href="/">
+//               <Image
+//                 src="/assets/Icons/logo-usa.png"
+//                 width={150}
+//                 height={80}
+//                 alt="logo"
+//               />
+//             </Link>
+//           </div>
+//           <div className="w-[400px] xl:w-[500px] 2xl:w-[850px] mx-[60px] xl:mx-[100px] 2xl:mx-[240px] py-1 2xl:py-20 relative z-10">
+//             <SearchBar />
+//             <div className="flex font-bold gap-10 items-center justify-between text-[8px] xl:text-[10px] 2xl:text-[12px] relative z-10 py-5">
+//               <div
+//                 className={`relative cursor-pointer`}
+//                 onClick={toggleBooksDropdown}
+//               >
+//                 <div
+//                   className={`flex gap-7 items-center ${
+//                     booksDropdownOpen ? "text-red-500" : ""
+//                   }`}
+//                 >
+//                   BOOKS{" "}
+//                   <div>{booksDropdownOpen ? <Upwards /> : <Downwards />}</div>{" "}
+//                 </div>
+//                 {booksDropdownOpen && <BookDropdown />}
+//               </div>
+//               {navItems.map((item, index) => (
+//                 <a key={index} href={item.url}>
+//                   <p className="cursor-pointer">{item.label}</p>
+//                 </a>
+//               ))}
+//             </div>
+//           </div>
+//           <Link href="/submit-online">
+//             <Button
+//               title="SUBMIT ONLINE"
+//               width="lg:w-[130px] 2xl:w-[140px]"
+//               color="red"
+//             />
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
--- src/components/common/navbar/SmNavbar.js
+++ src/components/common/navbar/SmNavbar.js
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import Image from "next/image";
 import Hamburger from "./comp/Hamburger";
@@ -14,7 +15,7 @@ const SmNavbar = () => {
   const { menuOpen, searchOpen } = useSmNavbarStore();
 
   return (
-    <>
+    <div className="sticky top-0 z-50">
       {searchOpen ? (
         <>
           <SearchBar />
@@ -42,8 +43,57 @@ const SmNavbar = () => {
       {searchOpen && <SearchMenu />}
 
       {menuOpen && <HamburgerMenu />}
-    </>
+    </div>
   );
 };
 
 export default SmNavbar;
+
+// import React from "react";
+// import Image from "next/image";
+// import Hamburger from "./comp/Hamburger";
+// import Cross from "./comp/Cross";
+// import Search from "./comp/Search";
+// import SearchBar from "./comp/SearchBar";
+// import SearchMenu from "./comp/SearchMenu";
+// import HamburgerMenu from "./comp/HamburgerMenu";
+// import { useSmNavbarStore } from "@/stores/States";
+// import Link from "next/link";
+
+// const SmNavbar = () => {
+//   const { menuOpen, searchOpen } = useSmNavbarStore();
+
+//   return (
+//     <div className="sticky top-0 z-50 ">
+//       {searchOpen ? (
+//         <>
+//           <SearchBar />
+//         </>
+//       ) : (
+//         <>
+//           <div className="py-5 bg-white flex items-center justify-between px-4">
+//             {!menuOpen && <Hamburger />}
+//             <div className="w-full flex justify-center items-center">
+//               <Link href="/">
+//                 <Image
+//                   src="/assets/Icons/logo-usa.png"
+//                   width={100}
+//                   height={75}
+//                   alt="logo"
+//                 />
+//               </Link>
+//             </div>
+//             {menuOpen && <Cross />}
+//             {!menuOpen && <Search />}
+//           </div>
+//         </>
+//       )}
+
+//       {searchOpen && <SearchMenu />}
+
+//       {menuOpen && <HamburgerMenu />}
+//     </div>
+//   );
+// };
+
+// export default SmNavbar;
