0c94d66bdc7c91db91e8f93451d16aa516249f38
hadees <hadees621@gmail.com>
2024-05-08 15:39:54 +0500

    fix: features/article mt

--- src/app/features/article/page.js
+++ src/app/features/article/page.js
@@ -1,10 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import { Books } from "@/utils/home/Books";
-import BookComponent from "@/components/home/BookComponent";
-import Button from "@/components/feature/Button";
-import AuthorComponent from "@/components/search/AuthorComponent";
 import RecentPost from "@/components/feature/RecentPost";
 import Facebook from "../../../../public/imgs/Facebook.svg";
 import Twitter from "../../../../public/imgs/Twitter.svg";
@@ -26,7 +22,7 @@ const page = () => {
 
   return (
     <div className="w-full mx-auto">
-      <div className="flex flex-col mx-auto lg:max-w-[900px] xl:max-w-[1000px] 2xl:max-w-[1400px]">
+      <div className="flex flex-col mx-auto lg:max-w-[900px] xl:max-w-[1000px] 2xl:max-w-[1400px] mt-28 lg:mt-0">
         <BackButton />
 
         <div className="pt-10 flex flex-col w-full justify-start md:justify-center font-open-sans space-y-5 p- lg:p-1 ">
