1b85584dd9f382b966568fe99fffd755cc403a25
hadees <hadees621@gmail.com>
2024-05-10 18:01:05 +0500

    refractor

--- src/app/authors/author/page.js
+++ src/app/authors/author/page.js
@@ -9,11 +9,11 @@ import Link from "next/link";
 
 const page = () => {
   return (
-    <div className="lg:max-w-[800px] xl:max-w-[1200px] 2xl:max-w-[1600px] mx-auto">
+    <div className="lg:max-w-[800px] xl:max-w-[1000px] 2xl:max-w-[1450px] mx-auto">
       <Link href={`/authors`}>
         <BackButton />
       </Link>
-      <div className="flex flex-col w-full justify-center md:justify-center lg:justify-start font-open-sans space-y-5 p-3 lg:p-1 px-3">
+      <div className="flex flex-col w-full justify-center md:justify-center lg:justify-start font-open-sans space-y-5 p-3 lg:p-1 px-3 ">
         <Banner />
         <AboutAuthor />
         <BooksSection title={"BOOKS"} lgTitle={"Books by Rudi Jennings"} />
